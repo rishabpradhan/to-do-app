4126967505d8b9de8585ec9ebc649d58c24d2575
rishab <rishabpradhan15@gmail.com>
2025-04-18 21:36:48 +0545

    to-do-code

--- backend/models/db.js
+++ backend/models/db.js
@@ -8,7 +8,6 @@ try {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
   });
   console.log("database has created and connected");
 } catch (err) {
