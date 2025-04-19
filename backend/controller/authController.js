6cecc205001e97fc039f57dc88b0a89ba76c25d7
rishab <rishabpradhan15@gmail.com>
2025-04-19 11:10:15 +0545

    some changes

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 const JWT_SECRET = process.env.JWT_SECRET;
